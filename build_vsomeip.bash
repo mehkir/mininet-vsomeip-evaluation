42c3f6c22854fe18a327b9cf11a3c96d03a8210f
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-11-21 17:42:37 +0100

    Add vsomeip build script

---
+++ build_vsomeip.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+cmake -B /home/mehmet/vscode-workspaces/mininet-vsomeip/vsomeip/build -S /home/mehmet/vscode-workspaces/mininet-vsomeip/vsomeip
+$(which cmake) --build /home/mehmet/vscode-workspaces/mininet-vsomeip/vsomeip/build --config Release --target all -- -j$(nproc)
+$(which cmake) --build /home/mehmet/vscode-workspaces/mininet-vsomeip/vsomeip/build --config Release --target examples -- -j$(nproc)
