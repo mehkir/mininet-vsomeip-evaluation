cb8be909b6553d618bc1db01679abc73f3d4cf85
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-01-08 19:11:19 +0100

    Add client zone file and reset command

--- reset-zone-file.bash
+++ reset-zone-file.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
 sed -i '/; =======================================================================/q' /home/mehmet/vscode-workspaces/mininet-vsomeip/zones/service.zone
+sed -i '/; =======================================================================/q' /home/mehmet/vscode-workspaces/mininet-vsomeip/zones/client.zone
