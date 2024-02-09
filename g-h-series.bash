0620dfc36239bb7e16f29e9319563ad9d9b670fb
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-02-09 14:26:37 +0100

    Start evaluation with time and log runtimes

--- g-h-series.bash
+++ g-h-series.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
-
+RUNTIMESLOG="runtimes.log"
+touch $RUNTIMESLOG
+RUNS=1
 for option in {G..H}; do
-    for host_count in {2..201}; do
-         echo "OPTION $option WITH $host_count hosts"
-        /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs 50
+    for host_count in {2..3}; do
+        $(which time) -a -o $RUNTIMESLOG -f "${option}-${host_count}-${RUNS}:\t%E real,\t%U user,\t%S sys"  /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs $RUNS
     done
 done
