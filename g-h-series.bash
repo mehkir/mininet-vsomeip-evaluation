4795269c6b630bac6c6da9e207a6b588bbb4d820
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-02-09 16:31:32 +0100

    Comment error detection, seems not working properly

--- g-h-series.bash
+++ g-h-series.bash
@@ -6,10 +6,10 @@ RUNS=10
 for option in {G..H}; do
     for ((host_count = 2; host_count <= UPPER_BOUND_HOSTS; host_count++)); do
         $(which time) -a -o $RUNTIMESLOG -f "${option}-${host_count}-${RUNS}:\t%E real,\t%U user,\t%S sys"  /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs $RUNS
-        exit_code=$?
-        if [ $exit_code ]; then
-            echo "Error: Non-zero exit code detected. Exiting."
-            break 2  # Break out of both loops
-        fi
+        # exit_code=$?
+        # if [ $exit_code ]; then
+        #     echo "Error: Non-zero exit code detected. Exiting."
+        #     break 2  # Break out of both loops
+        # fi
     done
 done
