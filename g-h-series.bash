565132d3ca355180a7d69bc48b1a4baeda456a3f
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-02-09 14:45:32 +0100

    Use for loop with variable

--- g-h-series.bash
+++ g-h-series.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 RUNTIMESLOG="runtimes.log"
+UPPER_BOUND_HOSTS=201
 touch $RUNTIMESLOG
-RUNS=1
+RUNS=10
 for option in {G..H}; do
-    for host_count in {2..3}; do
+    for ((host_count = 2; host_count <= UPPER_BOUND_HOSTS; host_count++)); do
         $(which time) -a -o $RUNTIMESLOG -f "${option}-${host_count}-${RUNS}:\t%E real,\t%U user,\t%S sys"  /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs $RUNS
     done
 done
