14010b5645c46413d7e48965188834893621fbbb
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-02-10 18:16:57 +0100

    perform mininet cleanup after every evaluation

--- g-h-series.bash
+++ g-h-series.bash
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 RUNTIMESLOG="runtimes.log"
-UPPER_BOUND_HOSTS=201
+UPPER_BOUND_HOSTS=151
 touch $RUNTIMESLOG
-RUNS=10
+RUNS=40
 for option in {G..H}; do
     for ((host_count = 2; host_count <= UPPER_BOUND_HOSTS; host_count++)); do
         $(which time) -a -o $RUNTIMESLOG -f "${option}-${host_count}-${RUNS}:\t%E real,\t%U user,\t%S sys"  /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs $RUNS
+        echo "Cleaning up mininet"
+        mn -c
+        echo "Done."
         # exit_code=$?
         # if [ $exit_code ]; then
         #     echo "Error: Non-zero exit code detected. Exiting."
