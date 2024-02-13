312e2bf478eda85252f6a4b7052f514a30ae9e2f
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2024-02-13 22:17:37 +0100

    Modify series bash, add runtimes, add full latency

--- series.bash
+++ series.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 RUNTIMESLOG="runtimes.log"
-UPPER_BOUND_HOSTS=151
+UPPER_BOUND_HOSTS=2
 touch $RUNTIMESLOG
-RUNS=40
-for option in {G..H}; do
+RUNS=50
+for option in {A..H}; do
     for ((host_count = 2; host_count <= UPPER_BOUND_HOSTS; host_count++)); do
         $(which time) -a -o $RUNTIMESLOG -f "${option}-${host_count}-${RUNS}:\t%E real,\t%U user,\t%S sys"  /home/mehmet/vscode-workspaces/mininet-vsomeip/.venv/bin/python topo-1sw-Nhosts.py --hosts $host_count --evaluate $option --runs $RUNS
         echo "Cleaning up mininet"
